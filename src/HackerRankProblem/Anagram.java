fe07bce9d16009b0235e01d1f5984732aaa3a812
Hari-mani-don <pselvamani486@gmail.com>
2024-02-12 09:35:52 -0500

    Add 3 file

--- src/HackerRankProblem/Anagram.java
+++ src/HackerRankProblem/Anagram.java
@@ -4,35 +4,40 @@ import java.util.Scanner;
 public class Anagram {
 
     static boolean isAnagram(String a, String b) {
-        char[] c =a.toCharArray();
-        char[] d =b.toCharArray();
+        char[] c = a.toCharArray();
+        char[] d = b.toCharArray();
 
         // Complete the function
-        for(int i=0;i<a.length();i++){
-            int first=0;
-            int second=0;
-            for(int j=0;j<a.length();j++){
-                if(c[i]==c[j] ){
-                    c[j]=0;
-                    System.out.print(c[j]);
-                    first++;
-                }
-            }
-            for (int k = 0; k < b.length(); k++) {
+        if (a.length() == b.length()) {
+            for (int i = 0; i < a.length(); i++) {
+                int first = 1;
+                int second = 0;
+                for (int j = i+1; j < a.length(); j++) {
+                    if (c[i] == c[j] && c[i]!=0  && c[j]!=0) {
+                        c[j] = 0;
+                        first++;
+                    }
+                }  //for j close
+                for (int k = 0; k < b.length(); k++) {
 
-                if(c[i]==d[k] ){
-                    d[k]=0;
-                    System.out.print(d[k]);
-                    second++;
+                    if (c[i] == d[k] && c[i]!=0 && d[k]!=0) {
+                        d[k] = 0;
+                        second++;
+                    }
+                } //for k close
+                if (first != second ) {
+                    return false;
                 }
-            }
-            if(first!=second){
-                System.out.println("hai hari");
-                return false;
-            }
+
+            }  // first for i close
+
+        } //if close
+        else{
+            return false;
         }
         return true;
-    }
+
+    } //anagram method close
 
     public static void main(String[] args) {
 
